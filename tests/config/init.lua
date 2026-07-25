333f598ab8b126fd3db3626e2800f915e7cd361f
Abhinandh S <abhinandhsuby@proton.me>
2026-07-25 23:15:09 +0530

    Update init.lua

--- tests/config/init.lua
+++ tests/config/init.lua
@@ -1,38 +1,4 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    {
-    'abhinandh-s/age.nvim',
-    cmd = { "Age" },
-    config = function()
-      require('age').setup({
-        encrypt_and_del = true,
-        key_file = vim.fn.expand("key.txt"),
-      })
-    end
-}
-  },
-})
-
+require("lazy")
 
 local success, err = pcall(function()
   vim.cmd("Age genkey")
